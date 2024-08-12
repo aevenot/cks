da467b4b41a50e94d4a8c0414267391d1a9e7942
aevenot <34191424+aevenot@users.noreply.github.com>
2024-08-12 11:00:41 +0200

    Update krew-setup.sh

--- krew-setup.sh
+++ krew-setup.sh
@@ -5,8 +5,8 @@
   KREW="krew-${OS}_${ARCH}" &&
   curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
   tar zxvf "${KREW}.tar.gz" &&
-  ./"${KREW}" install krew
-  export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+  ./"${KREW}" install krew &&
+  export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH" &&
   exec bash
 )
      
